dfd44effd59d068993db76a2768d23f6ac5c50c5
Stephan Druskat <mail@sdruskat.net>
2021-05-17 12:38:41 +0200

    Update test script to work with validation levels
    - Drop --schema arg
    - Introduce -s/--strict optional flag
    - Hard code schema file names
    - Introduce strict validation:
      - Build RefResolver to resolve "$ref"s to main schema file
      - Use resolver when doing strict validation
      - Output validation errors in a more readable way

--- schema_poc.py
+++ schema_poc.py
@@ -1,31 +1,66 @@
-import jsonschema
+from jsonschema import Draft7Validator, RefResolver
 import argparse
 from ruamel.yaml import YAML
 import json
 
 
-def validate(data_path, schema_path):
-    with open(data_path, 'r') as fi:
-        # Convert to Python object
-        yaml = YAML(typ='safe')
-        yml_data = yaml.load(fi)
+main_schema_file = 'schema.json'
+strict_schema_file = 'schema-strict.json'
 
-        with open(schema_path, 'r') as sf:
-            schema_data = json.loads(sf.read())
-            jsonschema.validate(instance=yml_data, schema=schema_data)
+
+def print_validation_result(validator, data):
+    if validator.is_valid(data):
+            print('Data is valid!')
+    else:
+        print('Data is invalid! Errors:')
+        for i, error in enumerate(sorted(validator.iter_errors(data), key=str)):
+            print('    ' + str(i + 1) + ': ' + error.message)
+
+
+def validate(data_path, strict_bool):
+    # Read main schema file, we need this in any case
+    with open(main_schema_file, 'r') as sf:
+        main_schema = json.loads(sf.read())
+
+    # Read YAML data, we need this in any case
+    with open(data_path, 'r') as fi:
+        yaml = YAML(typ='safe')
+        data = yaml.load(fi)
+
+    # Do we need strict validation?
+    if strict_bool:
+        print('Strict validation')
+        # Read strict schema
+        with open(strict_schema_file, 'r') as sf:
+            strict_schema = json.loads(sf.read())
+        schemastore = {}
+
+        # Store both schemas in local schemastore
+        schemastore[main_schema["$id"]] = main_schema
+        schemastore[strict_schema["$id"]] = strict_schema
+        assert len(schemastore) == 2
+
+        # Construct ref resolver
+        # https://python-jsonschema.readthedocs.io/en/stable/references/#jsonschema.RefResolver
+        resolver = RefResolver(
+            "file://schema-strict.json", referrer=strict_schema, store=schemastore)
+        print_validation_result(Draft7Validator(strict_schema, resolver=resolver), data)
+    else:
+        print('Lax validation')
+        print_validation_result(Draft7Validator(main_schema), data)
+        
 
 
 if __name__ == "__main__":
     """Run like this from the command line:
-    `python3 -m schema_poc.py -d data.yml -s schema.json`
+    `python3 -m schema_poc.py -d data.yml [-s]`
     """
-    parser = argparse.ArgumentParser(  
-        description='Validates a YAML file against a JSON Schema')  
-    parser.add_argument(  
-        '-d', '--data', type=str,  
+    parser = argparse.ArgumentParser(
+        description='Validates a YAML file against a JSON Schema')
+    parser.add_argument(
+        '-d', '--data', type=str,
         help='A YAML data file', required=True)
     parser.add_argument(
-        '-s', '--schema', type=str,
-        help='A YAML schema file', required=True) 
-    args = parser.parse_args()  
-    validate(args.data, args.schema)
+        '-s', '--strict', help='Turns on strict validation', action="store_true")
+    args = parser.parse_args()
+    validate(args.data, args.strict)
